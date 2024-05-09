77c9bc64a4bdca083b398d08e8a75ff0848e7401
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-05-09 17:43:45 -0400

    Updated through lab section 2.12

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,7 +5,7 @@ import Lab3 from "./Lab3";
 
 export default function Labs() {
     return (
-        <div>
+        <div id="wd-labs">
             <h1>Labs</h1>
             <Lab1 />
             <Lab2 />
