b26413d8e1a5b3894ce51f6ef4321fb3de575965
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-05-15 15:24:57 -0400

    Updated lecture 3 material based on studying

---
+++ src/Labs/Lab2/IDSelector.tsx
@@ -0,0 +1,15 @@
+export default function IDSelector() {
+    return (
+        <div id="wd-css-id-selectors">
+        <h3>ID selectors</h3>
+        <p id="id-selector-1">
+          Instead of changing the look and feel of all the elements of the same
+          name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="id-selector-2">
+          Here's another paragraph using a different ID and a different look and
+          feel
+        </p>
+      </div>
+    )
+}
---
+++ src/Labs/Lab2/StyleAttribute.tsx
@@ -0,0 +1,13 @@
+export default function StyleAttribute() {
+  return (
+    <div>
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+      <h3>Styling with the STYLE attribute</h3>
+      <p>
+        Style attribute allows configuring look and feel right on the element.
+        Although it's very convenient it is considered bad practice and you
+        should avoid using the style attribute
+      </p>
+    </div>
+  );
+}
