78c9f9969cc3d02573be78d2fe0cb4575198b486
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-05-16 20:43:23 -0400

    Updated module screen with grips on every item. Previous commit was bullet points under each learning objective

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -17,48 +17,61 @@ export default function Modules() {
             Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda
             <ModuleControlButtons />
           </div>
+
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical />
               <span className="wd-title">LEARNING OBJECTIVES</span>
               <LessonControlButtons />
-              <ul className="wd-content">
-                <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
-              </ul>
             </li>
+
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Introduction to the course
+              <LessonControlButtons />
+            </li>
+
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Learn what is Web Development
+              <LessonControlButtons />
+            </li>
+
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical />
-
               <span className="wd-title">READING</span>
               <LessonControlButtons />
-
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 1 - Introduction
-                </li>
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 2 - Creating User Interfaces
-                  With HTML
-                </li>
-              </ul>
             </li>
+
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Full Stack Developer - Chapter 1 - Introduction
+              <LessonControlButtons />
+            </li>
+
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Full Stack Developer - Chapter 2 - Creating User Interfaces With
+              HTML
+              <LessonControlButtons />
+            </li>
+
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical />
-
               <span className="wd-title">SLIDES</span>
               <LessonControlButtons />
+            </li>
 
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Introduction to Web Development Links to an external site.
-                </li>
-                <li className="wd-content-item">
-                  Creating an HTTP server with Node.js
-                </li>
-              </ul>
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Introduction to Web Development Links to an external site.
+              <LessonControlButtons />
+            </li>
+
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Creating an HTTP server with Node.js
+              <LessonControlButtons />
             </li>
           </ul>
         </li>
@@ -69,50 +82,53 @@ export default function Modules() {
             Week 1, Lecture 2 - Formatting User Interfaces with HTML
             <ModuleControlButtons />
           </div>
+
           <ul className="wd-lessons list-group rounded-0">
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical />
               <span className="wd-title ">LEARNING OBJECTIVES</span>
               <LessonControlButtons />
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Learn how to create user interfaces with HTML
-                </li>
-                <li className="wd-content-item">
-                  Deploy the assignment to Netlify
-                </li>
-              </ul>
+            </li>
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Learn how to create user interfaces with HTML
+              <LessonControlButtons />
+            </li>
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Deploy the assignment to Netlify
+              <LessonControlButtons />
             </li>
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical />
-
               <span className="wd-title">READING</span>
               <LessonControlButtons />
-
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 1 - Introduction
-                </li>
-                <li className="wd-content-item">
-                  Full Stack Developer - Chapter 2 - Creating User Interfaces
-                  With HTML
-                </li>
-              </ul>
+            </li>
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Full Stack Developer - Chapter 1 - Introduction
+              <LessonControlButtons />
+            </li>
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Full Stack Developer - Chapter 2 - Creating User Interfaces With
+              HTML
+              <LessonControlButtons />
             </li>
             <li className="wd-lesson list-group-item p-3 ps-1">
               <BsGripVertical />
-
               <span className="wd-title">SLIDES</span>
               <LessonControlButtons />
-
-              <ul className="wd-content">
-                <li className="wd-content-item">
-                  Introduction to HTML and the DOM
-                </li>
-                <li className="wd-content-item">
-                  Formatting Web content with Headings and Paragraphs
-                </li>
-              </ul>
+            </li>
+            <li className="wd-content-item list-group-item p-3 ps-3 ">
+              <BsGripVertical />
+              Introduction to HTML and the DOM
+              <LessonControlButtons />
+            </li>
+            <li className="wd-content-item list-group-item p-3 ps-3">
+              <BsGripVertical />
+              Formatting Web content with Headings and Paragraphs
+              <LessonControlButtons />
             </li>
           </ul>
         </li>
