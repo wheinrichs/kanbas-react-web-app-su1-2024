25d3c65278ccb173e6e2a9cf1524814b74930e79
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-05-20 17:58:27 -0400

    Updated and started lab 3

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,19 +1,64 @@
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputInline from "./ConditionalOuputInLine";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructuring";
+import House from "./House";
+import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreading";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import VariableTypes from "./VariableTypes";
+import VariablesAndConstants from "./VariablesAndConstants";
 import "./index.css";
-export default function Lab3() {
-    return (
-        <div>
-            <h2>Lab 3</h2>
-            <h1 id="asd" className="zxc">I'm white on red</h1>
+import TodoItem from "./todo/ToDoItem";
+import TodoList from "./todo/TodoList";
 
-            <div><input type="checkbox" name="radio"/>DSA</div>
-{/* <title><label type="htmlFor" name="radio"/>FDS</title>
-<title><label type="htmlFor" name="radio"/>GFD</title>  */}
-<label htmlFor="last">Last name</label>
-<input
-//   value="Brown"
-  placeholder="Lee"
-  id="last"
-  title="Type your last name"/>
-        </div>
-    );
+
+export default function Lab3() {
+  return (
+    <div id="wd-lab3">
+      <h2>Lab 3</h2>
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FindIndex />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+    </div>
+  );
 }
