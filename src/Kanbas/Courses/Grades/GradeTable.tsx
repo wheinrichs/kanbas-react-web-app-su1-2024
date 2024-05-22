0c00fef930312489f5a2b53a491fd5d89fd18699
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-05-22 18:48:58 -0400

    Finished grades screen

--- src/Kanbas/Courses/Grades/GradeTable.tsx
+++ src/Kanbas/Courses/Grades/GradeTable.tsx
@@ -1,4 +1,17 @@
+import { users, enrollments, grades, assignments } from "../../Database";
+import { useParams } from "react-router";
+
 export default function GradeTable() {
+  const { cid, id } = useParams();
+  const courseStudentsIDs = enrollments.filter(
+    (student) => student.course === cid
+  );
+  const courseStudents = users.filter((user) =>
+    courseStudentsIDs.find((ID) => ID.user === user._id)
+  );
+  const courseAssignments = assignments.filter(
+    (assignment) => assignment.course === cid
+  );
   return (
     <div>
       <div className="table-responsive">
@@ -8,128 +21,28 @@ export default function GradeTable() {
               <td>
                 <strong>Student Name</strong>
               </td>
-              <td>
-                A1 SETUP <br />
-                Out of 100
-              </td>
-              <td>
-                A2 HTML
-                <br /> Out of 100
-              </td>
-              <td>
-                A3 CSS
-                <br /> Out of 100
-              </td>
-              <td>
-                A4 BOOTSTRAP
-                <br /> Out of 100
-              </td>
+              {courseAssignments &&
+                courseAssignments.map((assignment) => (
+                  <td>
+                    {assignment._id} <br />
+                    Out of 100
+                  </td>
+                ))}
             </tr>
           </thead>
 
           <tbody>
-            <tr>
-                <td className="text-danger">
-                    Sam Smith
-                </td>
-                <td>
-                    <input type = "text" value = "98%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "13%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "54%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "33%" className="form-control border-0 bg-transparent"/>
-                </td>
-            </tr>
-            <tr>
-                <td className="text-danger">
-                    Jane Austin
-                </td>
-                <td>
-                    <input type = "text" value = "98%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "13%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "54%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "33%" className="form-control border-0 bg-transparent"/>
-                </td>
-            </tr>
-            <tr>
-                <td className="text-danger">
-                    Winston Heinrichs
-                </td>
-                <td>
-                    <input type = "text" value = "98%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "13%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "54%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "33%" className="form-control border-0 bg-transparent"/>
-                </td>
-            </tr>
-            <tr>
-                <td className="text-danger">
-                    Lexie Kremp
-                </td>
-                <td>
-                    <input type = "text" value = "98%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "13%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "54%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "33%" className="form-control border-0 bg-transparent"/>
-                </td>
-            </tr>
-            <tr>
-                <td className="text-danger">
-                    James Grey
-                </td>
-                <td>
-                    <input type = "text" value = "98%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "13%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "54%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "33%" className="form-control border-0 bg-transparent"/>
-                </td>
-            </tr>
-            <tr>
-                <td className="text-danger">
-                    Sarah Peak
-                </td>
-                <td>
-                    <input type = "text" value = "98%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "13%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "54%" className="form-control border-0 bg-transparent"/>
-                </td>
-                <td>
-                    <input type = "text" value = "33%" className="form-control border-0 bg-transparent"/>
-                </td>
-            </tr>
+            {courseStudents &&
+              courseStudents.map((student) => (
+                <tr>
+                  <td className="text-danger">
+                    {student.firstName} {student.lastName}
+                  </td>
+                  {courseAssignments.map((assignment) => (
+                   <td>{grades.find((grade) => student._id === grade.student && grade.assignment === assignment._id)?.grade}</td>
+                  ))}
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
