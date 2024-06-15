9527b15fa2929f120fd919e45b160f657e37f704
Windstorm <62821056+wheinrichs@users.noreply.github.com>
2024-06-15 17:46:05 -0400

    Implemented the course database mongo

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
-const COURSES_API = `${REMOTE_SERVER}/api/courses`;
+export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+export const COURSES_API = `${REMOTE_SERVER}/api/courses`;
 
 export const fetchAllCourses = async () => {
-  const { data } = await axios.get(COURSES_API);
-  return data;
+  const data = await axios.get(COURSES_API);
+  return data.data;
 };
 
 export const createCourse = async (course: any) => {
@@ -14,11 +14,13 @@ export const createCourse = async (course: any) => {
   
   export const deleteCourse = async (id: any) => {
     const response = await axios.delete(`${COURSES_API}/${id}`);
+    console.log("Response: ", response.data);
     return response.data;
   };
   
   export const updateCourse = async (course: any) => {
-    const response = await axios.put(`${COURSES_API}/${course.id}`, course);
+    //console.log("Course info: ", course.id);
+    const response = await axios.put(`${COURSES_API}/${course._id}`, course);
     return response.data;
   };
   
